65a2227f75d6ebc24daf0ef2b74ad19f3dc6e2d9
JPGrandclementMirakl <jean-patrick.grandclement@mirakl.com>
2026-09-10 00:59:04 +0200

    feat: remplacer BrowserRouter par HashRouter et supprimer le basename du routage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { ContextualCursor } from "./components/ContextualCursor";
 import { NavCapsule } from "./components/NavCapsule";
 import { SealReveal } from "./components/SealReveal";
@@ -21,7 +21,7 @@ export default function App() {
   useLenis();
 
   return (
-    <Router basename="/fire-cream">
+    <Router>
       <div className="layout-container">
         <ContextualCursor />
         <NavCapsule />
@@ -45,3 +45,4 @@ export default function App() {
     </Router>
   );
 }
+
